ac1bdb84e63542bf0e68db79e1c628a109b1c546
Albeiro Cuadrado <albeiro.cuadrado@simetrik.com>
2021-10-21 02:30:49 -0500

    Add Airflow, dags, update models and some refactors

---
+++ cloud2/dags/convertir.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from datetime import timedelta
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from cloud2.conversor.conversor import Conversor
+
+# These args will get passed on to the python operator
+default_args = {
+    'owner': 'MISO_21',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    'email': ['a.cuadrado@uniandes.edu.co'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'convertor',
+    default_args=default_args,
+    description='Process all upload files',
+    schedule_interval='*/5 * * * *'
+)
+
+task = PythonOperator(
+    task_id='print',
+    python_callable=Conversor().convert(),
+    dag=dag,
+)
